821aed08e26f6fdadef248f1da2e109bb0f1d056
howardboland <howard.boland@gmail.com>
2014-03-12 15:11:40 +0000

    Flash banner - dynamic changing for each level #55

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -3,6 +3,18 @@
 angular.module('knock2winApp')
   .controller('GameCtrl', ['$scope', '$state', function ($scope, $state) {
 
+    $scope.bannerCounter = 0;
+    $scope.banners = [{ logoURL: '/banners/logo-ee.png', clickTag: 'http://shop.ee.co.uk/lg-g2-group/pay-monthly/details/' },
+                     { logoURL: '/banners/logo-carphonewarehouse.png', clickTag: 'http://www.carphonewarehouse.com/mobiles/mobile-phones/LG_G2/MONTHLY?colourCode=WHITE' },
+                     { logoURL: '/banners/logo-o2.png', clickTag: 'https://www.o2.co.uk/shop/phones/lg/g2/' },
+                     { logoURL: '/banners/logo-3.png', clickTag: 'http://www.three.co.uk/Discover/Devices/LG/G2' }];
+    $scope.banner =  $scope.banners[ $scope.bannerCounter ];
+    
+    $scope.nextbanner = function()
+    {
+        $scope.bannerCounter = ($scope.bannerCounter + 1) % $scope.banners.length;
+        $scope.banner =  $scope.banners[ $scope.bannerCounter ];
+    }
     $scope.message = "";
     $scope.level = 1;
     $scope.maxlevel = 4;
@@ -13,6 +25,7 @@ angular.module('knock2winApp')
     }
     $scope.next = function()
     {
+        $scope.nextbanner()
         if ($scope.level<$scope.maxlevel)
         {
             $scope.level++;
@@ -27,6 +40,7 @@ angular.module('knock2winApp')
     $scope.restart = function()
     {
         var promise = $state.transitionTo("game.play");
+        $scope.nextbanner();
     }
 
     $scope.ismobile = detectmob();
